f77a55135ae66769df9cff0cb2e492243e16d228
Ripster47 <mpolinski12@gmail.com>
2019-03-01 09:29:47 -0600

    seed dumps

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Recipe.create!([
+  {title: "Krabby Patty", chef: "King Neptune", ingredients: "Love, Not magic", directions: "Read to the pickles, slow char the patty, nestle all ingredients between buns", image_url: "https://i.ytimg.com/vi/k5e1HPeusiA/hqdefault.jpg", prep_time: 20},
+  {title: "Les Poisson", chef: "Louie", ingredients: "A sweet little succulent crab", directions: "I stuff you with bread, it wont hurt cuz youre dead!", image_url: "https://media.giphy.com/media/LbJUWVnUzQQ9O/giphy.gif", prep_time: 3},
+  {title: "Lasagna", chef: "Garfield", ingredients: "noodles, sauce, cheese, beef", directions: "john cooks it, I eat it", image_url: "https://media.giphy.com/media/zLzGdW9Eav1JK/giphy.gif", prep_time: 400},
+  {title: "Cookies", chef: "Cookie Monster", ingredients: "cookie mix, milk, eggs, love", directions: "cookie, cookie, cookie, cookie, darn, should have bought eggs", image_url: "https://i.gifer.com/7H9L.gif", prep_time: 400},
+  {title: "Tiny Fried Eggs", chef: "Little Bits", ingredients: "Tiny eggs", directions: "Get tiny eggs, Tiny fry them, Eat some fuckin shit you fuckin stupid bitch", image_url: "https://thumbs.gfycat.com/OffbeatCorruptGrison-max-1mb.gif", prep_time: 1},
+  {title: "Big Kahuna Burger", chef: "Jules", ingredients: "One Tasty Burger", directions: "Ask someone if you can try a bite of their delicious burger, Ask for some sprite to wash it down, Shoot them fucking dead", image_url: "https://i.ytimg.com/vi/dRyD74DRheI/maxresdefault.jpg", prep_time: 5}
+])
+
